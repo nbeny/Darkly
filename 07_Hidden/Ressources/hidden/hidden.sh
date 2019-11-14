ab653cb3cdffb82e1ee03f43e9d3613bcc7494ad
42aroger <aroger@student.42.fr>
2019-11-14 11:28:21 +0100

    fix script + usage

--- 07_Hidden/Ressources/hidden/hidden.sh
+++ 07_Hidden/Ressources/hidden/hidden.sh
@@ -1,19 +1,58 @@
 #!/bin/bash
-url='http://10.0.2.8/.hidden/'
+if [ -z "$1" ]
+then
+	echo 'Usage : ./hidden.sh ip'
+	exit 0
+fi
+url="http://$1/.hidden/"
 wget $url'/README'
+line=`cat README`
+if [[ "$line" =~ \ |\' ]]
+then
+   echo ""
+else
+   echo $line >> file.txt
+fi
+rm README
 for elema in "amcbevgondgcrloowluziypjdh" "bnqupesbgvhbcwqhcuynjolwkm" "ceicqljdddshxvnvdqzzjgddht" "doxelitrqvhegnhlhrkdgfizgj" "eipmnwhetmpbhiuesykfhxmyhr" "ffpbexkomzbigheuwhbhbfzzrg" "ghouhyooppsmaizbmjhtncsvfz" "hwlayeghtcotqdigxuigvjufqn" "isufpcgmngmrotmrjfjonpmkxu" "jfiombdhvlwxrkmawgoruhbarp" "kpibbgxjqnvrrcpczovjbvijmz" "ldtafmsxvvydthtgflzhadiozs" "mrucagbgcenowkjrlmmugvztuh" "ntyrhxjbtndcpjevzurlekwsxt" "oasstobmotwnezhscjjopenjxy" "ppjxigqiakcrmqfhotnncfqnqg" "qcwtnvtdfslnkvqvzhjsmsghfw" "rlnoyduccpqxkvcfiqpdikfpvx" "sdnfntbyirzllbpctnnoruyjjc" "trwjgrgmfnzarxiiwvwalyvanm" "urhkbrmupxbgdnntopklxskvom" "viphietzoechsxwqacvpsodhaq" "whtccjokayshttvxycsvykxcfm" "xuwrcwjjrmndczfcrmwmhvkjnh" "yjxemfsgdlkbvvtjiylhdoaqkn" "zzfzjvjsupgzinctxeqtzzdzll"
 do
 	#echo $elema
 	wget $url$elema'/README'
+	line=`cat README`
+	if [[ "$line" =~ \ |\' ]]
+	then
+	   echo ""
+	else
+	   echo $line >> file.txt
+	fi
+	rm README
 	for elemb in "acbnunauucfplzmaglkvqgswwn" "bvwrujeymrvzurvywnjxzlfkwa" "ccevyakvydrjhsvbnwvestcfeb" "dephqnhvretuprssiccazdpwyt" "eotxvxzbogrepmvuiplzkfjohm" "fnkbjkxzduuctvrzpvpdsllkwm" "gubyklkxvljikilfdqyajypgpt" "hycgkytgbrqobqkozszhfgmven" "igeemtxnvexvxezqwntmzjltkt" "jzqhwxudbzrxyesccqbirteemr" "keyczixybfxybczctwbarfcjhk" "lacqgphmpkmzjmaojyqnasjyvj" "mrbnakzcmpldxxsjjssyujjvbx" "nvvgvrrnuepeduqsfwrcocoixo" "odgxyhuvecqvvfpzvtermzwisa" "pyvqjseoycohylldbjajacgwgx" "qihvsavsvodnsrnwhwxsjcwscf" "rtenpnkzuftuclrqrglitjsrgk" "sbkitppotcdimedtxzwlrocxbm" "ttlemtrngbjvrxotdxihcbhdzu" "uuqwurkperbaipglabcxpwcogm" "vsjtwjnsblouvdzmhzwwfiwimv" "wxkyoqkiafgluzmfpgcthpainy" "xpvwxitxurnldvlkeyedmlsson" "ycdpkqpcolzyfwsyjfehsolqvq" "zcgkxuyzzplsfnisngzlayvgee"
 	do
 		#echo $elemb
 		wget $url$elema'/'$elemb'/README'
+	        line=`cat README`
+        	if [[ "$line" =~ \ |\' ]]
+        	then
+			echo ""
+		else
+           		echo $line >> file.txt
+      		fi
+		rm README
 		for elemc in "ayuprpftypqspruffmkuucjccv" "becskiwlclcuqxshqmxhicouoj" "cqqssunxyhjgdwjoafgyzoollx" "dupoqdxhvrbqhaqokxsiigjnph" "emdxzqwvfkmkjvfbyxizowjlqr" "ftzcgojutitjfpqrdadyfewfov" "gtmgedazcchqobjyuufjkxmmam" "hrgjwugrgpxlrwntddjeoizipk" "iumzgolywwwsdqbunmlkagpfqu" "juavephzegfusfrqelvumphzat" "kbjjgbfcbchslgysntmtmcxzyr" "lmpanswobhwcozdqixbowvbrhw" "mfmtemmsbpftlvuuuwitbydbbt" "nzzuqitxumdibwksdfdbczvahq" "oehtfkmejiwsbvoqyztwllaqqb" "pupwvwozdhgnvmzdktffjxfiqc" "qtbemrfggdbetcjaiyvwxagqhn" "raetkuxexbsoiywlcceelgpkdl" "shdualhcrcmmzslakailyvnbbt" "tojkymyisskfbmweakfvwghyqs" "uwohzpdobnmnlwhfgmhsjnvtss" "vpaznrumfdlwgbxuqnfmunthun" "wqjklbcbceqvyecbsaityellly" "xhytouigdvshzvldngdskfmkpf" "yivtvgtfhotbwchtwottzwghaa" "zrhmbyumtnjbgoiwkksmroifhb"
 		do
 			#echo $elemc
 			wget $url$elema'/'$elemb'/'$elemc'/README'
+		        line=`cat README`
+        		if [[ "$line" =~ \ |\' ]]
+        		then
+				echo ""
+			else
+           			echo $line >> file.txt
+        		fi
+			rm README
 		done
 	done
 done
-cat READ* | grep f
+echo -e "\nThe flag is :\n"
+cat file.txt
+rm file.txt
